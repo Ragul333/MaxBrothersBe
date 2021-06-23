42d3141e27a97cfbdd76c28cdf3b245646862d63
Ragul S <s.ragul3333@gmail.com>
2021-06-23 17:44:15 +0530

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { createMovies } from "../controllers/movieController.js";
 import { getMovies, getMovie } from "../controllers/posts.js";
-import { createProjects } from "../controllers/projectControllers.js";
+import {
+  createProjects,
+  getProjects,
+} from "../controllers/projectControllers.js";
 
 const router = express.Router();
 
@@ -9,6 +12,7 @@ router.route("/").get(getMovies);
 router.route("/:id").get(getMovie);
 router.route("/post").post(createMovies);
 router.route("/postproject").post(createProjects);
+router.route("/postproject/projects").get(getProjects);
 /* router.get("/:id", getPost);
 router.delete("/post/:id", deletePost); */
 
