99a03d10ee9cf1c377efe362fea263a64417e8a9
Ragul333 <s.ragul3333@gmail.com>
2021-07-09 20:17:06 +0530

    Upload in cloudinary

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import fileUpload from "express-fileupload";
 import movieRoutes from "./routes/posts.js";
 import userRoutes from "./routes/userRoute.js";
+import uploadRoutes from "./routes/upload.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
 dotenv.config();
 app.use(cors());
 
 app.use(express.json({ limit: "50mb", extended: true }));
 
+app.use(fileUpload({
+  useTempFiles:true
+}))
+
 app.use("/movies", movieRoutes);
 app.use("/movies", userRoutes);
+app.use("/movies/upload", uploadRoutes);
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
@@ -29,4 +39,6 @@ mongoose
   )
   .catch((error) => console.log(error));
 
+
+  
 mongoose.set("useFindAndModify", false);
---
+++ routes/upload.js
@@ -0,0 +1,51 @@
+import express from "express";
+const router = express.Router();
+import cloudinary from "cloudinary";
+import fs from 'fs'
+
+
+cloudinary.config({
+  cloud_name: 'drpwuzvax',
+  api_key: '639564898932956',
+  api_secret: 'ErJ8fJbn3WY2nanTo8nrLFJoFes',
+});
+
+router.post("/image", (req, res) => {
+  try {
+    if(!req.files || Object.keys(req.files).length === 0){
+      removeTmp(file.tempFilePath)
+      return res.status(400).json({message:'No files'})
+    }
+    
+    const file = req.files.file;
+    
+    if(file.size>1024*1024){
+      removeTmp(file.tempFilePath)
+      return res.status(400).json({message:'Large file'})
+    } 
+    
+    if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+      removeTmp(file.tempFilePath)
+       return res.status(400).json({message:'Informat file'}) 
+    }
+  
+    
+    cloudinary.v2.uploader.upload(file.tempFilePath, {folder:"test"}, async(err,result)=>{
+      if(err) throw err;
+      res.json({public_id: result.public_id, url: result.secure_url})
+    })
+
+    
+  } catch (error) {
+    res.status(500).send(error);
+  }  
+});
+
+const removeTmp = (path) => {
+  fs.unlink(path, err =>{
+      if(err) throw err;
+
+  })
+}
+
+export default router;
